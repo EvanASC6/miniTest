0e58d948e3d79a93beb758a0cd8c4b5f7fcfbd0f
EvanASC6 <emockpc@gmail.com>
2019-07-22 15:57:23 -0400

    boom

--- test.js
+++ test.js
@@ -1,34 +1,61 @@
 // 1. Declare a number called myNumber that is equal to your age and print it
-
+let myNumber = 16;
+console.log(myNumber);
 
 
 // 2. Declare a string called myName that contains your name and print it
-
+let myName = "Evan Mock";
+console.log(myName);
 
 
 // 3. Declare an array named myArray that contains your age, your first name, and if it's true or false that you're a student
-
+let myArray = [myNumber, myName, Boolean(true)];
+console.log(myArray);
 
 
 // 4. Write and call a function called printHello that prints your name to the console by accessing myArray
-
+let printHello = myArray[1];
+console.log(printHello);
 
 
 /* 5.
 1)create a function called area that takes in two parameters, width and length
 2) the function, area should return width times length
 3) call the function and give two values for the parameters */
+function area(){
+    let width = process.argv[2];
+    let length = process.argv[3];
+    let final = width * length;
+    return final
 
-
+}
+console.log(area())
 
 /* 6. Use conditional statements to write fizzBuzz
 1) if num is divisble by BOTH 3 and 5, console.log "FizzBuzz"
 2) if num is divisible by 3, console.log "Fizz"
 3)  if num is divisible by 5, console.log "Buzz"  */
+let number = process.argv[4]
 
+if (number % 3 == 0 && number % 5 == 0){
+    console.log("FizzBuzz");
+}
+
+else if (number % 5 == 0){
+     console.log("Buzz");
+}
+
+else if (number % 3 == 0){
+    console.log("Fizz");
+}
+else{
+    console.log( number + " is not divisable by either");
+}
 
 // 7. Write a loop that prints hello to the console 4 times
-
+for (let i=0; i<5; i++){
+    console.log("Hello");
+}
 
 
 //8. write a nested for loop that reads the array and console.log each element
@@ -41,21 +68,21 @@ let nestedArray = [
 
 //9a. make a directory called test
 
-// write answer here: 
+// write answer here: mkdir test
 
 //9b. move to your test directory that you just created
 
-// write answer here: 
+// write answer here: cd test
 
 //9c. make a file called test.js
 
-// write answer here: 
+// write answer here: touch test.js
 
 //9d. what is the command to open a file to vscode? 
 
-// write answer here: 
+// write answer here: code test.js
 
 //9e. So you already did "git add ." and then you did git commit -m
 // What is the last step of the process to push everything to github?
 
-// write answer here: 
+// write answer here: .$ git push
